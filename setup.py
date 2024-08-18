0364360a5d1b3098c42444641b5c4fa10a0db503
Suraj Yadav <qwertypad7612@gmail.com>
2024-08-18 12:08:34 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Emotion detection from text',
+    author='Suraj',
+    license='MIT',
+)
